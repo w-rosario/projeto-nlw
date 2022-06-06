f03db3e365d2e53978988faab80ede3c7e8ac620
weslley <weslley.rosario07@gmail.com>
2022-06-06 11:05:42 -0300

    update 2022.6.6

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,11 +1,18 @@
 import {VscGithubInverted} from 'react-icons/vsc'
 import styles from './styles.module.scss';
+import {useContext, useEffect} from "react";
+import {api} from "../../services/api";
+import {AuthContext} from "../../contexts/auth";
+
+
 
 export function LoginBox() {
+    const {signInUrl, user} = useContext(AuthContext);
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24"/>
                 Entrar com GitHub
             </a>
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './styles/global.css'
+import {AuthProvider} from "./contexts/auth";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
   </React.StrictMode>
 )
